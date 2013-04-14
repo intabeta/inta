d6f3af86fade92928e266584bfc089e722c661c1
Christopher <christopher.shriver@gmail.com>
2013-04-14 13:26:16 -0400

    updated variable name
    added _1 under decayedposts.py

--- content/management/commands/decayedposts.py
+++ content/management/commands/decayedposts.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from django.core.management.base import BaseCommand, CommandError
 from content.models import InterestGroup, IgProposal, IgProposalForm, Entry
 import os
@@ -35,3 +36,42 @@ class Command(BaseCommand):
     
 
         self.stdout.write('Success\n')
+=======
+from django.core.management.base import BaseCommand, CommandError
+from content.models import InterestGroup, IgProposal, IgProposalForm, Entry
+import os
+from datetime import datetime, timedelta
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Decay for entry scores.'
+    def handle(self, *args, **options):
+        entries = Entry.objects.all()
+        for entry in entries:
+            total = entry.posts + entry.double_posts * 2
+            now = datetime.utcnow()
+            then = entry.date_added.replace(tzinfo=None)
+            lapsed = now - then
+            print lapsed.total_seconds()
+            entry.decayed_score_1 = total * (1 / 1.15 ** (lapsed.total_seconds() / 28800))
+            print total, entry.decayed_score_1
+            entry.save()
+            
+            
+            
+            
+#             total = entry.posts + entry.double_posts * 2
+#             if entry.last_score == 0:
+#                 entry.last_score=1
+#             entry.last_growth = ((total - entry.last_score)/entry.last_score) * 100
+#             if entry.last_growth > 0.0:
+#                 print entry, total, entry.last_growth
+#             entry.last_score = total
+#             entry.save()
+#             
+            
+            
+    
+
+        self.stdout.write('Success\n')
+>>>>>>> updated variable name
