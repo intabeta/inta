56640c1ec2e667c49b3401b0843d57908e980eba
Christopher <christopher.shriver@gmail.com>
2013-04-17 20:58:14 -0400

    trying to pull a tagval with a specific tag name; might have to split into more than one line

--- content/views.py
+++ content/views.py
@@ -282,15 +282,15 @@ def tag_list(request, tags, method):
                     tagnew = Tag.objects.get(name=taglist[0])
                     tagnew_name = tagnew.name
                     post_change.voted_by.voter_set.create(tag=tagnew, user=user, val=1, slug=post_slug)
-                    post_change.posts.tagval_set.get(tag_name=tagnew_name).val += 1
-                    post_change.decayed_score_1.tagval_set.get(tag_name=tagnew_name).val += 1
-                    post_change.decayed_score_2.tagval_set.get(tag_name=tagnew_name).val += 1
-                    post_change.decayed_score_3.tagval_set.get(tag_name=tagnew_name).val += 1
-                    post_change.decayed_score_4.tagval_set.get(tag_name=tagnew_name).val += 1
-                    post_change.decayed_score_5.tagval_set.get(tag_name=tagnew_name).val += 1
-                    post_change.decayed_score_6.tagval_set.get(tag_name=tagnew_name).val += 1
-                    post_change.decayed_score_7.tagval_set.get(tag_name=tagnew_name).val += 1
-                    post_change.decayed_score_8.tagval_set.get(tag_name=tagnew_name).val += 1
+                    post_change.posts.tagval_set.get(tag.name=tagnew_name).val += 1
+                    post_change.decayed_score_1.tagval_set.get(tag.name=tagnew_name).val += 1
+                    post_change.decayed_score_2.tagval_set.get(tag.name=tagnew_name).val += 1
+                    post_change.decayed_score_3.tagval_set.get(tag.name=tagnew_name).val += 1
+                    post_change.decayed_score_4.tagval_set.get(tag.name=tagnew_name).val += 1
+                    post_change.decayed_score_5.tagval_set.get(tag.name=tagnew_name).val += 1
+                    post_change.decayed_score_6.tagval_set.get(tag.name=tagnew_name).val += 1
+                    post_change.decayed_score_7.tagval_set.get(tag.name=tagnew_name).val += 1
+                    post_change.decayed_score_8.tagval_set.get(tag.name=tagnew_name).val += 1
                     post_change.save()
 
                     # if request.user.is_authenticated():
