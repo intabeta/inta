f974f51e97713b88346f1e9d85dfd35b781b9de5
Christopher <christopher.shriver@gmail.com>
2013-04-20 19:24:54 -0400

    add scores next to tags on entries and sort them

--- content/views.py
+++ content/views.py
@@ -382,11 +382,12 @@ def tag_list(request, tags, method):
 ##            posts = sorted(entries.filter(date_added__range=(datetime.now() - timedelta(days=365), datetime.now() - timedelta(days=6))), key=lambda a: -a.ranking)
 
         votecounts = [a._get_ranking(taglist[0]) for a in posts]
+        tagscores = [ sorted([ tag.name, post._get_ranking(tag) for tag in post.tags.all()], key=lambda a: -a[1]) for post in posts]
         toprelevant = sorted([[tag.name,sum([a._get_ranking(tag) for a in posts])] for tag in Tag.objects.all()], key=lambda a: -a[1])[:10]
         toptags = sorted([[tag.name,sum([a._get_ranking(tag) for a in Entry.objects.all()])]for tag in Tag.objects.all()], key=lambda a: -a[1])[:10] #get top ten tags by number of votes over all entries
         template_data = {
             'tags': tags,
-            'postdata': zip(posts,votecounts),
+            'postdata': zip(posts,votecounts,tagscores),
             'voter': voter,
             'double_voter': double_voter,
             'method': method,
@@ -436,9 +437,10 @@ def tag_list(request, tags, method):
         votecounts = [a._get_ranking(taglist[0]) for a in posts]
         toptags = sorted([[tag.name,sum([a._get_ranking(tag) for a in Entry.objects.all()])] for tag in Tag.objects.all()], key=lambda a: -a[1])[:10] #get top ten tags by number of votes over all entries
         toprelevant = sorted([[tag.name,sum([a._get_ranking(tag) for a in posts])] for tag in Tag.objects.all()], key=lambda a: -a[1])[:10] #get top related tags (ranked by votes only, not by how closely related they are)
+        tagscores = [ sorted([ tag.name, post._get_ranking(tag) for tag in post.tags.all()], key=lambda a: -a[1]) for post in posts]
         template_data = {
             'tags': tags,
-            'postdata': zip(posts,votecounts),
+            'postdata': zip(posts,votecounts,tagscores),
             'method': method,
             'taglist': taglist,
             'toptags': toptags,
