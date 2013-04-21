a544f070f067623b16c6faf30cd1bf28cae6f825
Christopher <christopher.shriver@gmail.com>
2013-04-21 18:01:27 -0400

    write command to calculate top tags

---
+++ content/management/commands/toptags.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from content.models import Entry, Dict
+from taggit.models import Tag
+import os
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Updates top tags Dict (id=193, name=PaqubeSTUPhACh8trust62Ex6Ve5am).'
+    def handle(self, *args, **options):
+        toptags = Dict.objects.get(id=193)
+        #empty old toptags dict
+        for tagval in toptags.tagval_set.all():
+            tagval.delete()
+
+        #calculate new top tags and place in dict
+        newtoptags = sorted([[tag.name,sum([a._get_ranking(tag) for a in Entry.objects.all()])]for tag in Tag.objects.all()], key=lambda a: -a[1])
+        for tagval in newtoptags:
+            toptags.tagval_set.create(tag=tagval[0], val=tagval[1])
+
+        print('New top tags:\n'+sum(tagval.name+': '+str(tagval.val)+'\n'))
