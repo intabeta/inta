054f4e1d247c911523bbbafb2c248c8ce9f6a401
Christopher <christopher.shriver@gmail.com>
2013-04-21 02:37:39 -0400

    it's not necessary to save the entry here, hopefully removing this line will speed it up a bit

--- content/management/commands/decayedposts.py
+++ content/management/commands/decayedposts.py
@@ -42,5 +42,4 @@ class Command(BaseCommand):
                 tval8.save()
                 #print total
                 #print(entry.title+'; '+tagnew.name)
-                entry.save()
         self.stdout.write('Success\n')
