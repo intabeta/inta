989e9ce49d8b23071ae2374a4cefedfbf3ff9c42
Christopher <christopher.shriver@gmail.com>
2013-04-27 16:11:16 -0400

    rearrange code in toptags so that it deletes old toptags lists right before making the new ones. otherwise you'll have a few seconds where the lists are empty

--- content/management/commands/toptags.py
+++ content/management/commands/toptags.py
@@ -17,28 +17,8 @@ class Command(BaseCommand):
         toptags_d6 = Dict.objects.get(id=199)
         toptags_d7 = Dict.objects.get(id=200)
         toptags_d8 = Dict.objects.get(id=201)
-        
-        #empty old toptags dicts
-        for tagval in toptags.tagval_set.all():
-            tagval.delete()
-        for tagval in toptags_d1.tagval_set.all():
-            tagval.delete()
-        for tagval in toptags_d2.tagval_set.all():
-            tagval.delete()
-        for tagval in toptags_d3.tagval_set.all():
-            tagval.delete()
-        for tagval in toptags_d4.tagval_set.all():
-            tagval.delete()
-        for tagval in toptags_d5.tagval_set.all():
-            tagval.delete()
-        for tagval in toptags_d6.tagval_set.all():
-            tagval.delete()
-        for tagval in toptags_d7.tagval_set.all():
-            tagval.delete()
-        for tagval in toptags_d8.tagval_set.all():
-            tagval.delete()
 
-        #calculate new top tags and place in dict
+        #calculate new top tags, empty old ones, and put in the new
         newtoptags = sorted([[tag.name,sum([a._get_ranking(tag) for a in Entry.objects.all()])] for tag in Tag.objects.all()], key=lambda a: -a[1])
         newtoptags_d1 = sorted([[tag.name,sum([ a._get_ranking(tag,'decay1') for a in Entry.objects.all()])] for tag in Tag.objects.all()], key=lambda a: -a[1])
         newtoptags_d2 = sorted([[tag.name,sum([ a._get_ranking(tag,'decay2') for a in Entry.objects.all()])] for tag in Tag.objects.all()], key=lambda a: -a[1])
@@ -49,22 +29,48 @@ class Command(BaseCommand):
         newtoptags_d7 = sorted([[tag.name,sum([ a._get_ranking(tag,'decay7') for a in Entry.objects.all()])] for tag in Tag.objects.all()], key=lambda a: -a[1])
         newtoptags_d8 = sorted([[tag.name,sum([ a._get_ranking(tag,'decay8') for a in Entry.objects.all()])] for tag in Tag.objects.all()], key=lambda a: -a[1])
 
+        for tagval in toptags.tagval_set.all():
+            tagval.delete()
         for tagval in newtoptags:
             toptags.tagval_set.create(tag=tagval[0], val=int(tagval[1]))
+        
+        for tagval in toptags_d1.tagval_set.all():
+            tagval.delete()
         for tagval in newtoptags_d1:
             toptags_d1.tagval_set.create(tag=tagval[0], val=int(tagval[1]))
+        
+        for tagval in toptags_d2.tagval_set.all():
+            tagval.delete()
         for tagval in newtoptags_d2:
             toptags_d2.tagval_set.create(tag=tagval[0], val=int(tagval[1]))
+
+        for tagval in toptags_d3.tagval_set.all():
+            tagval.delete()
         for tagval in newtoptags_d3:
             toptags_d3.tagval_set.create(tag=tagval[0], val=int(tagval[1]))
+
+        for tagval in toptags_d4.tagval_set.all():
+            tagval.delete()
         for tagval in newtoptags_d4:
             toptags_d4.tagval_set.create(tag=tagval[0], val=int(tagval[1]))
+
+        for tagval in toptags_d5.tagval_set.all():
+            tagval.delete()
         for tagval in newtoptags_d5:
             toptags_d5.tagval_set.create(tag=tagval[0], val=int(tagval[1]))
+
+        for tagval in toptags_d6.tagval_set.all():
+            tagval.delete()
         for tagval in newtoptags_d6:
             toptags_d6.tagval_set.create(tag=tagval[0], val=int(tagval[1]))
+
+        for tagval in toptags_d7.tagval_set.all():
+            tagval.delete()
         for tagval in newtoptags_d7:
             toptags_d7.tagval_set.create(tag=tagval[0], val=int(tagval[1]))
+
+        for tagval in toptags_d8.tagval_set.all():
+            tagval.delete()
         for tagval in newtoptags_d8:
             toptags_d8.tagval_set.create(tag=tagval[0], val=int(tagval[1]))
 
