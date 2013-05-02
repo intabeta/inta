2bcb80293cc336959566b3990d0ace730652859e
Christopher <christopher.shriver@gmail.com>
2013-05-02 19:49:26 -0400

    allow data to be empty

--- content/models.py
+++ content/models.py
@@ -56,7 +56,7 @@ class IgProposal(models.Model):
 
 class DataList(models.Model):
     name = models.CharField(max_length=100)
-    data = models.CommaSeparatedIntegerField(max_length=500)
+    data = models.CommaSeparatedIntegerField(max_length=500, null=True)
 
     def __unicode__(self):
         return self.name
