da8916822130e62de3afc910ca810816e802e931
Christopher <christopher.shriver@gmail.com>
2013-05-02 21:29:27 -0400

    start working on a function to calculate top posts

---
+++ content/management/commands/rankposts.py
@@ -0,0 +1,25 @@
+from django.core.management.base import BaseCommand, CommandError
+from content.models import Entry, Dict, DataList
+from taggit.models import Tag
+import os
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Updates top tags Dict (id=193, name=PaqubeSTUPhACh8trust62Ex6Ve5am).'
+    def handle(self, *args, **options):
+        activetagids = DataList.objects.get(id=1)
+        activetags = [ Tag.objects.get(id=val).name for val in activetagids ]
+
+        print('active tags:')
+        for tag in activetags:
+            print(tag)
+            relevantposts = Entry.objects.filter(tags__name__in=[tag])
+            relevantposts_votes = sorted(relevantposts, key=lambda a: -a._get_ranking(tag))
+            top = DataList.objects.get(name='top_'+tag)
+            top.data = relevantposts_votes
+            top.save()
+
+        for entry in DataList.objects.get(name='top_test'):
+            print(entry.name,entry._get_ranking(test))
+            
+
