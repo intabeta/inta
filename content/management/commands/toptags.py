86bb2575e0fd1c1468e4796d56affc69dcee1ac6
Christopher <christopher.shriver@gmail.com>
2013-05-06 15:37:05 -0400

    filter out Entries that have the specific tag instead of looping over all of them

--- content/management/commands/toptags.py
+++ content/management/commands/toptags.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = 'Updates top tags Dict (id=193, name=PaqubeSTUPhACh8trust62Ex6Ve5am).'
     def handle(self, *args, **options):
         toptags = Dict.objects.get(id=193)
-        newtoptags = [[tag.name,sum([a._get_ranking(tag) for a in Entry.objects.all()])] for tag in Tag.objects.all()]
+        newtoptags = [[tag.name,sum([a._get_ranking(tag) for a in Entry.objects.filter(tags__name__in=[tag])])] for tag in Tag.objects.all()]
         for tagval in toptags.tagval_set.all():
             tagval.delete()
         for tagval in newtoptags:
@@ -16,7 +16,7 @@ class Command(BaseCommand):
         del toptags,newtoptags
 
         toptags_d1 = Dict.objects.get(id=194)
-        newtoptags_d1 = [[tag.name,sum([ a._get_ranking(tag,'decay1') for a in Entry.objects.all()])] for tag in Tag.objects.all()]
+        newtoptags_d1 = [[tag.name,sum([ a._get_ranking(tag,'decay1') for a in Entry.objects.filter(tags__name__in=[tag])])] for tag in Tag.objects.all()]
         for tagval in toptags_d1.tagval_set.all():
             tagval.delete()
         for tagval in newtoptags_d1:
@@ -24,7 +24,7 @@ class Command(BaseCommand):
         del toptags_d1,newtoptags_d1
 
         toptags_d2 = Dict.objects.get(id=195)
-        newtoptags_d2 = [[tag.name,sum([ a._get_ranking(tag,'decay2') for a in Entry.objects.all()])] for tag in Tag.objects.all()]
+        newtoptags_d2 = [[tag.name,sum([ a._get_ranking(tag,'decay2') for a in Entry.objects.filter(tags__name__in=[tag])])] for tag in Tag.objects.all()]
         for tagval in toptags_d2.tagval_set.all():
             tagval.delete()
         for tagval in newtoptags_d2:
@@ -32,7 +32,7 @@ class Command(BaseCommand):
         del toptags_d2,newtoptags_d2
 
         toptags_d3 = Dict.objects.get(id=196)
-        newtoptags_d3 = [[tag.name,sum([ a._get_ranking(tag,'decay3') for a in Entry.objects.all()])] for tag in Tag.objects.all()]
+        newtoptags_d3 = [[tag.name,sum([ a._get_ranking(tag,'decay3') for a in Entry.objects.filter(tags__name__in=[tag])])] for tag in Tag.objects.all()]
         for tagval in toptags_d3.tagval_set.all():
             tagval.delete()
         for tagval in newtoptags_d3:
@@ -40,7 +40,7 @@ class Command(BaseCommand):
         del toptags_d3,newtoptags_d3
 
         toptags_d4 = Dict.objects.get(id=197)
-        newtoptags_d4 = [[tag.name,sum([ a._get_ranking(tag,'decay4') for a in Entry.objects.all()])] for tag in Tag.objects.all()]
+        newtoptags_d4 = [[tag.name,sum([ a._get_ranking(tag,'decay4') for a in Entry.objects.filter(tags__name__in=[tag])])] for tag in Tag.objects.all()]
         for tagval in toptags_d4.tagval_set.all():
             tagval.delete()
         for tagval in newtoptags_d4:
@@ -48,7 +48,7 @@ class Command(BaseCommand):
         del toptags_d4,newtoptags_d4
 
         toptags_d5 = Dict.objects.get(id=198)
-        newtoptags_d5 = [[tag.name,sum([ a._get_ranking(tag,'decay5') for a in Entry.objects.all()])] for tag in Tag.objects.all()]
+        newtoptags_d5 = [[tag.name,sum([ a._get_ranking(tag,'decay5') for a in Entry.objects.filter(tags__name__in=[tag])])] for tag in Tag.objects.all()]
         for tagval in toptags_d5.tagval_set.all():
             tagval.delete()
         for tagval in newtoptags_d5:
@@ -56,7 +56,7 @@ class Command(BaseCommand):
         del toptags_d5,newtoptags_d5
 
         toptags_d6 = Dict.objects.get(id=199)
-        newtoptags_d6 = [[tag.name,sum([ a._get_ranking(tag,'decay6') for a in Entry.objects.all()])] for tag in Tag.objects.all()]
+        newtoptags_d6 = [[tag.name,sum([ a._get_ranking(tag,'decay6') for a in Entry.objects.filter(tags__name__in=[tag])])] for tag in Tag.objects.all()]
         for tagval in toptags_d6.tagval_set.all():
             tagval.delete()
         for tagval in newtoptags_d6:
@@ -64,7 +64,7 @@ class Command(BaseCommand):
         del toptags_d6,newtoptags_d6
 
         toptags_d7 = Dict.objects.get(id=200)
-        newtoptags_d7 = [[tag.name,sum([ a._get_ranking(tag,'decay7') for a in Entry.objects.all()])] for tag in Tag.objects.all()]
+        newtoptags_d7 = [[tag.name,sum([ a._get_ranking(tag,'decay7') for a in Entry.objects.filter(tags__name__in=[tag])])] for tag in Tag.objects.all()]
         for tagval in toptags_d7.tagval_set.all():
             tagval.delete()
         for tagval in newtoptags_d7:
@@ -72,7 +72,7 @@ class Command(BaseCommand):
         del toptags_d7,newtoptags_d7
 
         toptags_d8 = Dict.objects.get(id=201)
-        newtoptags_d8 = [[tag.name,sum([ a._get_ranking(tag,'decay8') for a in Entry.objects.all()])] for tag in Tag.objects.all()]
+        newtoptags_d8 = [[tag.name,sum([ a._get_ranking(tag,'decay8') for a in Entry.objects.filter(tags__name__in=[tag])])] for tag in Tag.objects.all()]
         for tagval in toptags_d8.tagval_set.all():
             tagval.delete()
         for tagval in newtoptags_d8:
