3ea89d7ed3167668d198ee0f8ce3482170490cf3
Christopher <christopher.shriver@gmail.com>
2013-05-06 22:32:04 -0400

    update rankposts to make a DataList for every sort function

--- content/management/commands/rankposts.py
+++ content/management/commands/rankposts.py
@@ -5,22 +5,51 @@ import os
 
 class Command(BaseCommand):
     args = ''
-    help = 'Updates top tags Dict (id=193, name=PaqubeSTUPhACh8trust62Ex6Ve5am).'
+    help = 'Updates top tags Dict (id=193, name=top_votes).'
     def handle(self, *args, **options):
-        activetagids = eval(DataList.objects.get(id=1).data)
+        activetaglist = DataList.objects.get(id=1)
+        activetagids = eval(activetaglist.data)
+        activetaglist.data = []
+        activetaglist.save()
         activetags = [ Tag.objects.get(id=val).name for val in activetagids ]
 
-        print('active tags:')
         for tag in activetags:
-            print(tag)
             relevantposts = Entry.objects.filter(tags__name__in=[tag])
             relevantposts_votes = sorted(relevantposts, key=lambda a: -a._get_ranking(tag))
-            top = DataList.objects.get(name='top_'+tag)
-            top.data = [ post.id for post in relevantposts_votes ]
-            top.save()
-
-        for entryid in eval(DataList.objects.get(name='top_test').data):
-            entry = Entry.objects.get(id=entryid)
-            print(entry.title,entry._get_ranking('test'))
+            votes, c = DataList.objects.get_or_create(name='top_'+tag) #this function returns a tuple; the variable c is either True or False depending on whether a new object was created
+            votes.data = [ post.id for post in relevantposts_votes ]
+            votes.save()
+            relevantposts_d1 = sorted(relevantposts, key=lambda a: -a._get_ranking(tag,'decay1'))
+            d1, c = DataList.objects.get_or_create(name='top_d1_'+tag)
+            d1.data = [ post.id for post in relevantposts_d1 ]
+            d1.save()
+            relevantposts_d2 = sorted(relevantposts, key=lambda a: -a._get_ranking(tag,'decay2'))
+            d2, c = DataList.objects.get_or_create(name='top_d2_'+tag)
+            d2.data = [ post.id for post in relevantposts_d2 ]
+            d2.save()
+            relevantposts_d3 = sorted(relevantposts, key=lambda a: -a._get_ranking(tag,'decay3'))
+            d3, c = DataList.objects.get_or_create(name='top_d3_'+tag)
+            d3.data = [ post.id for post in relevantposts_d3 ]
+            d3.save()
+            relevantposts_d4 = sorted(relevantposts, key=lambda a: -a._get_ranking(tag,'decay4'))
+            d4, c = DataList.objects.get_or_create(name='top_d4_'+tag)
+            d4.data = [ post.id for post in relevantposts_d4 ]
+            d4.save()
+            relevantposts_d5 = sorted(relevantposts, key=lambda a: -a._get_ranking(tag,'decay5'))
+            d5, c = DataList.objects.get_or_create(name='top_d5_'+tag)
+            d5.data = [ post.id for post in relevantposts_d5 ]
+            d5.save()
+            relevantposts_d6 = sorted(relevantposts, key=lambda a: -a._get_ranking(tag,'decay6'))
+            d6, c = DataList.objects.get_or_create(name='top_d6_'+tag)
+            d6.data = [ post.id for post in relevantposts_d6 ]
+            d6.save()
+            relevantposts_d7 = sorted(relevantposts, key=lambda a: -a._get_ranking(tag,'decay7'))
+            d7, c = DataList.objects.get_or_create(name='top_d7_'+tag)
+            d7.data = [ post.id for post in relevantposts_d7 ]
+            d7.save()
+            relevantposts_d8 = sorted(relevantposts, key=lambda a: -a._get_ranking(tag,'decay8'))
+            d8, c = DataList.objects.get_or_create(name='top_d8_'+tag)
+            d8.data = [ post.id for post in relevantposts_d8 ]
+            d8.save()
             
 
