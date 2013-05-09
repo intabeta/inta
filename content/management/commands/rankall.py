88622b41294abdcff687de532589b19ec1929b55
Christopher <christopher.shriver@gmail.com>
2013-05-09 01:14:10 -0400

    forgot to save list of ids instead of list of entries

--- content/management/commands/rankall.py
+++ content/management/commands/rankall.py
@@ -12,45 +12,45 @@ class Command(BaseCommand):
 
         topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag) for tag in tags]))
         top = DataList.objects.get(id=2)
-        top.data = topposts
+        top.data = [ post.id for post in topposts ]
         top.save()
 
         topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay1') for tag in tags]))
         top = DataList.objects.get(id=3)
-        top.data = topposts
+        top.data = [ post.id for post in topposts ]
         top.save()
 
         topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay2') for tag in tags]))
         top = DataList.objects.get(id=4)
-        top.data = topposts
+        top.data = [ post.id for post in topposts ]
         top.save()
 
         topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay3') for tag in tags]))
         top = DataList.objects.get(id=5)
-        top.data = topposts
+        top.data = [ post.id for post in topposts ]
         top.save()
 
         topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay4') for tag in tags]))
         top = DataList.objects.get(id=6)
-        top.data = topposts
+        top.data = [ post.id for post in topposts ]
         top.save()
 
         topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay5') for tag in tags]))
         top = DataList.objects.get(id=7)
-        top.data = topposts
+        top.data = [ post.id for post in topposts ]
         top.save()
 
         topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay6') for tag in tags]))
         top = DataList.objects.get(id=8)
-        top.data = topposts
+        top.data = [ post.id for post in topposts ]
         top.save()
 
         topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay7') for tag in tags]))
         top = DataList.objects.get(id=9)
-        top.data = topposts
+        top.data = [ post.id for post in topposts ]
         top.save()
 
         topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay8') for tag in tags]))
         top = DataList.objects.get(id=10)
-        top.data = topposts
+        top.data = [ post.id for post in topposts ]
         top.save()
