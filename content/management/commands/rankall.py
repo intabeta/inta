419626267a6206b6a6e1562f24e8f283d00e0bdd
Christopher <christopher.shriver@gmail.com>
2013-05-09 02:32:27 -0400

    speed up rankall command

--- content/management/commands/rankall.py
+++ content/management/commands/rankall.py
@@ -10,47 +10,47 @@ class Command(BaseCommand):
         tags = Tag.objects.all()
         entries = Entry.objects.all()
 
-        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag) for tag in tags]))
+        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag) for tag in a.tags.all()]))
         top = DataList.objects.get(id=2)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay1') for tag in tags]))
+        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay1') for tag in a.tags.all()]))
         top = DataList.objects.get(id=3)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay2') for tag in tags]))
+        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay2') for tag in a.tags.all()]))
         top = DataList.objects.get(id=4)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay3') for tag in tags]))
+        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay3') for tag in a.tags.all()]))
         top = DataList.objects.get(id=5)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay4') for tag in tags]))
+        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay4') for tag in a.tags.all()]))
         top = DataList.objects.get(id=6)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay5') for tag in tags]))
+        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay5') for tag in a.tags.all()]))
         top = DataList.objects.get(id=7)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay6') for tag in tags]))
+        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay6') for tag in a.tags.all()]))
         top = DataList.objects.get(id=8)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay7') for tag in tags]))
+        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay7') for tag in a.tags.all()]))
         top = DataList.objects.get(id=9)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay8') for tag in tags]))
+        topposts = sorted(entries, key=lambda a: -sum([ a._get_ranking(tag,'decay8') for tag in a.tags.all()]))
         top = DataList.objects.get(id=10)
         top.data = [ post.id for post in topposts ]
         top.save()
