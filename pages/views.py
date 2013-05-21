84706b8d32577e74ed64d7ef4e5669c36a36e966
Christopher <christopher.shriver@gmail.com>
2013-05-20 22:32:20 -0500

    list comprehension mistake

--- pages/views.py
+++ pages/views.py
@@ -257,8 +257,8 @@ def taglist(request, tags='', method='decay3', domain='', page=1,
     if tags == '':
         taglist = [ tag.name for tag in Tag.objects.all() ]
     else:
-        taglist = [ tag in tags.split('+') if not site_re.re_match(tag) ]
-        domainlist = [ tag in tags.split('+') if site_re.re_match(tag) ]
+        taglist = [ tag for tag in tags.split('+') if not site_re.re_match(tag) ]
+        domainlist = [ tag for tag in tags.split('+') if site_re.re_match(tag) ]
         tags = '+'.join(taglist)
         domain = domainlist[0][5:] if domainlist else ''
     
