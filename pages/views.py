1906e7592e67fe83440e2294de21e3ce2acc7ad8
Christopher <christopher.shriver@gmail.com>
2013-05-29 02:25:00 -0500

    accidentally lost unicode to string conversion

--- pages/views.py
+++ pages/views.py
@@ -219,7 +219,7 @@ def splash(request):
 
 def graphtest(request):
     tags = Tag.objects.all()
-    tagnames = [ t.name for t in tags ]
+    tagnames = [ str(t.name) for t in tags ]
     entries = Entry.objects.all()
     edges=[]
     tagscores = dict()
@@ -236,7 +236,7 @@ def graphtest(request):
     for e in edges:
         edges2.append([tagnames.index(e[0]),tagnames.index(e[1]),e[2]])
     n = len(tags)
-    points= [ [200+100*cos(2*pi*i/n),200+100*sin(2*pi*i/n),tagnames[i],tagscores[tags[i].name]/6] for i in range(n) ]
+    points= [ [200+100*cos(2*pi*i/n),200+100*sin(2*pi*i/n),tagnames[i],tagscores[tagnames[i]]/6] for i in range(n) ]
             
     template_data = {
         'points': points,
