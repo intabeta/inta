eb35766818ec4737dcae19be113c151bbf8b3751
Christopher <christopher.shriver@gmail.com>
2013-05-29 00:39:17 -0500

    break ties in rankall by which post is newer (i.e. has a higher id)

--- content/management/commands/rankall.py
+++ content/management/commands/rankall.py
@@ -10,47 +10,47 @@ class Command(BaseCommand):
         tags = Tag.objects.all()
         entries = Entry.objects.all()
 
-        topposts = sorted(entries, key=lambda a: -a.score)
+        topposts = sorted(entries, key=lambda a: (-a.score,-a.id))
         top = DataList.objects.get(id=2)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -a.score_d1)
+        topposts = sorted(entries, key=lambda a: (-a.score_d1,-a.id))
         top = DataList.objects.get(id=3)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -a.score_d2)
+        topposts = sorted(entries, key=lambda a: (-a.score_d2,-a.id))
         top = DataList.objects.get(id=4)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -a.score_d3)
+        topposts = sorted(entries, key=lambda a: (-a.score_d3,-a.id))
         top = DataList.objects.get(id=5)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -a.score_d4)
+        topposts = sorted(entries, key=lambda a: (-a.score_d4,-a.id))
         top = DataList.objects.get(id=6)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -a.score_d5)
+        topposts = sorted(entries, key=lambda a: (-a.score_d5,-a.id))
         top = DataList.objects.get(id=7)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -a.score_d6)
+        topposts = sorted(entries, key=lambda a: (-a.score_d6,-a.id))
         top = DataList.objects.get(id=8)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -a.score_d7)
+        topposts = sorted(entries, key=lambda a: (-a.score_d7,-a.id))
         top = DataList.objects.get(id=9)
         top.data = [ post.id for post in topposts ]
         top.save()
 
-        topposts = sorted(entries, key=lambda a: -a.score_d8)
+        topposts = sorted(entries, key=lambda a: (-a.score_d8,-a.id))
         top = DataList.objects.get(id=10)
         top.data = [ post.id for post in topposts ]
         top.save()
