5d270323cc9a9c2b977e255ce10cd977b404a725
Christopher <christopher.shriver@gmail.com>
2013-05-30 17:04:40 -0500

    use tag.name instead of tag

--- content/management/commands/decayedposts.py
+++ content/management/commands/decayedposts.py
@@ -25,35 +25,35 @@ class Command(BaseCommand):
             for tagnew in entry.tags.all():
                 total = entry._get_ranking(tagnew) #posts + 2*double_posts
                 entry.score += total
-                tval1=entry.decayed_score_1.tagval_set.get(tag__iexact=tagnew)
+                tval1=entry.decayed_score_1.tagval_set.get(tag__iexact=tagnew.name)
                 tval1.val = round(total * (base ** (lapsed.total_seconds() / 1800)),5)
                 entry.score_d1 += tval1.val
                 tval1.save()
-                tval2=entry.decayed_score_2.tagval_set.get(tag__iexact=tagnew)
+                tval2=entry.decayed_score_2.tagval_set.get(tag__iexact=tagnew.name)
                 tval2.val = round(total * (base ** (lapsed.total_seconds() / 10800)),5)
                 entry.score_d2 += tval2.val
                 tval2.save()
-                tval3=entry.decayed_score_3.tagval_set.get(tag__iexact=tagnew)
+                tval3=entry.decayed_score_3.tagval_set.get(tag__iexact=tagnew.name)
                 tval3.val = round(total * (base ** (lapsed.total_seconds() / 86400)),5)
                 entry.score_d3 += tval3.val
                 tval3.save()
-                tval4=entry.decayed_score_4.tagval_set.get(tag__iexact=tagnew)
+                tval4=entry.decayed_score_4.tagval_set.get(tag__iexact=tagnew.name)
                 tval4.val = round(total * (base ** (lapsed.total_seconds() / 259200)),5)
                 entry.score_d4 += tval4.val
                 tval4.save()
-                tval5=entry.decayed_score_5.tagval_set.get(tag__iexact=tagnew)
+                tval5=entry.decayed_score_5.tagval_set.get(tag__iexact=tagnew.name)
                 tval5.val = round(total * (base ** (lapsed.total_seconds() / 604800)),5)
                 entry.score_d5 += tval5.val
                 tval5.save()
-                tval6=entry.decayed_score_6.tagval_set.get(tag__iexact=tagnew)
+                tval6=entry.decayed_score_6.tagval_set.get(tag__iexact=tagnew.name)
                 tval6.val = round(total * (base ** (lapsed.total_seconds() / 2592000)),5)
                 entry.score_d6 += tval6.val
                 tval6.save()
-                tval7=entry.decayed_score_7.tagval_set.get(tag__iexact=tagnew)
+                tval7=entry.decayed_score_7.tagval_set.get(tag__iexact=tagnew.name)
                 tval7.val = round(total * (base ** (lapsed.total_seconds() / 7776000)),5)
                 entry.score_d7 += tval7.val
                 tval7.save()
-                tval8=entry.decayed_score_8.tagval_set.get(tag__iexact=tagnew)
+                tval8=entry.decayed_score_8.tagval_set.get(tag__iexact=tagnew.name)
                 tval8.val = round(total * (base ** (lapsed.total_seconds() / 31536000)),5)
                 entry.score_d8 += tval8.val
                 tval8.save()
