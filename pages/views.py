ce4351b168d6c919ced692fd8956217b4fb39757
Christopher <christopher.shriver@gmail.com>
2013-06-04 03:30:33 -0500

    Revert "still not working… i'll look at it more tomorrow"
    This reverts commit 061f75a0852ad190358dd0a9a5a9ee4c29ab2e49.

--- pages/views.py
+++ pages/views.py
@@ -865,8 +865,8 @@ def taglist(request, tags='', method='decay3', domain='', page=1,
         if tags:
             slugs = [ entry.slug for entry in posts ]
             voters = user.voter_set.filter(slug__in=slugs)
-            voter = [ voter.slug for voter in voters.filter(val=1) if voter.tag == taglist[0] ]
-            double_voter = [ voter.slug for voter in voters.filter(val=2) if voter.tag == taglist[0] ]
+            voter = [ voter.slug for voter in voters.filter(val=1) if voter.tag == taglist[0]]
+            double_voter = [ voter.slug for voter in voters.filter(val=2) if voter.tag == taglist[0]]
         else:
             votetags = [ ts[0][0] for ts in tagscores ]
             for entry, tag in zip(posts, votetags):
