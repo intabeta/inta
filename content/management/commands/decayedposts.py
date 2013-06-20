253e4286e270ba438b758d74bb0f9e2157709dc3
Christopher <christopher.shriver@gmail.com>
2013-06-20 17:32:53 -0500

    move toptags calculation to decayedposts and use a better algorithm, split decayedposts into different commands to be called at different intervals

--- content/management/commands/decayedposts.py
+++ content/management/commands/decayedposts.py
@@ -1,7 +1,23 @@
 from django.core.management.base import BaseCommand, CommandError
-from content.models import InterestGroup, IgProposal, IgProposalForm, Entry
+from content.models import Entry, Dict
+from collections import defaultdict
 import os
 from datetime import datetime, timedelta
+
+def top(ls, key):
+	ans=ls[0]
+	for item in ls:
+		if key(item)>key(ans):
+			ans=item
+	return ans
+
+def topn(ls, n, key):
+	ans=[]
+	for i in range(n):
+		x=top(ls,key)
+		ls.remove(x)
+		ans.append(x)
+	return ans
 
 class Command(BaseCommand):
     args = ''
@@ -9,13 +25,20 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         entries = Entry.objects.all()
         base = 0.5
+        tagscores = defaultdict(int)
+        tagscores_d2 = defaultdict(int)
+        tagscores_d3 = defaultdict(int)
+        tagscores_d4 = defaultdict(int)
+        tagscores_d5 = defaultdict(int)
+        tagscores_d6 = defaultdict(int)
+        tagscores_d7 = defaultdict(int)
+        tagscores_d8 = defaultdict(int)
         for entry in entries:
             now = datetime.utcnow()
             then = entry.date_added.replace(tzinfo=None)
             lapsed = now - then
             entry.score = 0
 
-            d1 = base ** (lapsed.total_seconds()/1800)
             d2 = base ** (lapsed.total_seconds()/10800)
             d3 = base ** (lapsed.total_seconds()/86400)
             d4 = base ** (lapsed.total_seconds()/259200)
@@ -24,7 +47,6 @@ class Command(BaseCommand):
             d7 = base ** (lapsed.total_seconds()/7776000)
             d8 = base ** (lapsed.total_seconds()/31536000)
 
-            entry.d1 = round(d1,4)
             entry.d2 = round(d2,4)
             entry.d3 = round(d3,4)
             entry.d4 = round(d4,4)
@@ -36,7 +58,64 @@ class Command(BaseCommand):
             for tag in entry.tags.all():
                 total = entry._get_ranking(tag) #posts + 2*double_posts
                 entry.score += total
+                tagscores[tag] += total
+                tagscores_d2[tag] += total*d2
+                tagscores_d3[tag] += total*d3
+                tagscores_d4[tag] += total*d4
+                tagscores_d5[tag] += total*d5
+                tagscores_d6[tag] += total*d6
+                tagscores_d7[tag] += total*d7
+                tagscores_d8[tag] += total*d8
                 #print total
                 #print(entry.title+'; '+tagnew.name)
             entry.save()
+        toptags = topn(list(tagscores.items()), 20, lambda i: i[1])
+        dl = Dict.objects.get(id=193)
+        for tv in dl.tagval_set.all():
+            tv.delete()
+        for tv in toptags:
+            dl.tagval_set.create(tag=tv[0],val=tv[1])
+
+        toptags = topn(list(tagscores_d2.items()), 20, lambda i: i[1])
+        dl = Dict.objects.get(id=195)
+        for tv in dl.tagval_set.all():
+            tv.delete()
+        for tv in toptags:
+            dl.tagval_set.create(tag=tv[0],val=tv[1])
+        toptags = topn(list(tagscores_d3.items()), 20, lambda i: i[1])
+        dl = Dict.objects.get(id=196)
+        for tv in dl.tagval_set.all():
+            tv.delete()
+        for tv in toptags:
+            dl.tagval_set.create(tag=tv[0],val=tv[1])
+        toptags = topn(list(tagscores_d4.items()), 20, lambda i: i[1])
+        dl = Dict.objects.get(id=197)
+        for tv in dl.tagval_set.all():
+            tv.delete()
+        for tv in toptags:
+            dl.tagval_set.create(tag=tv[0],val=tv[1])
+        toptags = topn(list(tagscores_d5.items()), 20, lambda i: i[1])
+        dl = Dict.objects.get(id=198)
+        for tv in dl.tagval_set.all():
+            tv.delete()
+        for tv in toptags:
+            dl.tagval_set.create(tag=tv[0],val=tv[1])
+        toptags = topn(list(tagscores_d6.items()), 20, lambda i: i[1])
+        dl = Dict.objects.get(id=199)
+        for tv in dl.tagval_set.all():
+            tv.delete()
+        for tv in toptags:
+            dl.tagval_set.create(tag=tv[0],val=tv[1])
+        toptags = topn(list(tagscores_d7.items()), 20, lambda i: i[1])
+        dl = Dict.objects.get(id=200)
+        for tv in dl.tagval_set.all():
+            tv.delete()
+        for tv in toptags:
+            dl.tagval_set.create(tag=tv[0],val=tv[1])
+        toptags = topn(list(tagscores_d8.items()), 20, lambda i: i[1])
+        dl = Dict.objects.get(id=201)
+        for tv in dl.tagval_set.all():
+            tv.delete()
+        for tv in toptags:
+            dl.tagval_set.create(tag=tv[0],val=tv[1])
         self.stdout.write('Success\n')
---
+++ content/management/commands/decayposts_d1.py
@@ -0,0 +1,52 @@
+from django.core.management.base import BaseCommand, CommandError
+from content.models import Entry, Dict
+from collections import defaultdict
+import os
+from datetime import datetime, timedelta
+
+def top(ls, key):
+	ans=ls[0]
+	for item in ls:
+		if key(item)>key(ans):
+			ans=item
+	return ans
+
+def topn(ls, n, key):
+	ans=[]
+	for i in range(n):
+		x=top(ls,key)
+		ls.remove(x)
+		ans.append(x)
+	return ans
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Decay for entry scores.'
+    def handle(self, *args, **options):
+        entries = Entry.objects.all()
+        base = 0.5
+        tagscores_d1 = defaultdict(int)
+        now = datetime.utcnow()
+        for entry in entries:
+            then = entry.date_added.replace(tzinfo=None)
+            lapsed = now - then
+
+            if lapsed.total_seconds < 7200: #restrict decay1 to looking at posts under 2 hours old
+                d1 = base ** (lapsed.total_seconds()/1800)
+
+                entry.score = 0
+                entry.d1 = round(d1,4)
+                
+                for tag in entry.tags.all():
+                    score = entry._get_ranking(tag) #posts + 2*double_posts
+                    entry.score += score
+                    tagscores_d1[tag] += score*d1
+                entry.save()
+
+        toptags = topn(list(tagscores_d1.items()), 20, lambda i: i[1])
+        dl = Dict.objects.get(id=194)
+        for tv in dl.tagval_set.all():
+            tv.delete()
+        for tv in toptags:
+            dl.tagval_set.create(tag=tv[0],val=tv[1])
+        self.stdout.write('Success\n')
