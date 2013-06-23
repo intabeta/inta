ebef73940c4a13b9f686623ba0bf56fdea37f7f1
Christopher <christopher.shriver@gmail.com>
2013-06-23 17:00:00 -0500

    forgot to import defaultdict

--- content/management/commands/updategraph.py
+++ content/management/commands/updategraph.py
@@ -1,7 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
-from content.models import Entry, Dict, Graph
+from content.models import Entry, Graph
 from taggit.models import Tag
-from math import sin, cos, pi
+from collections import defaultdict
 import os
 
 class Command(BaseCommand):
