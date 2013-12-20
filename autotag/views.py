3974e55601684a894b186056f3625519c07630f7
Christopher <christopher.shriver@gmail.com>
2013-12-20 16:47:21 -0500

    fix views function

--- autotag/views.py
+++ autotag/views.py
@@ -1,5 +1,6 @@
 from autotag.keywords import getkeywords
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def autotag(request):
     "Demo autotag functionality"
@@ -11,7 +12,7 @@ def autotag(request):
         keywords = getkeywords(url)
 
         template_data = {'keywords':str(keywords)}
-        return render_to_response('autotag.html', template_data)
+        return render_to_response('autotag.html', template_data, context_instance=RequestContext(request))
     else:
         template_data = {'keywords':'none'}
-        return render_to_response('autotag.html', template_data)
+        return render_to_response('autotag.html', template_data, context_instance=RequestContext(request))
