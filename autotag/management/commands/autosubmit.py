2399d4358d09bd268d7e32c06a9e4cfa20987148
Christopher <christopher.shriver@gmail.com>
2014-01-20 18:29:09 -0500

    fix function name

--- autotag/management/commands/autosubmit.py
+++ autotag/management/commands/autosubmit.py
@@ -1,9 +1,9 @@
 from django.core.management.base import BaseCommand, CommandError
-from autotag.rss import rss_submit, urls
+from autotag.rss import submit_rss, urls
 import os
 
 class Command(BaseCommand):
     args = ''
     help = 'Autosubmits from rss'
     def handle(self, *args, **options):
-        rss_submit(urls)
+        submit_rss(urls)
